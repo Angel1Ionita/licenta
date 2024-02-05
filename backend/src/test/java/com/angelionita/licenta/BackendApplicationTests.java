3fb2e36ef821695cd3adea6731bf9aebe8267d28
Angel1Ionita <ionita_angel_24@yahoo.com>
2024-02-05 15:54:47 +0200

    initial setup

---
+++ backend/src/test/java/com/angelionita/licenta/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelionita.licenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
